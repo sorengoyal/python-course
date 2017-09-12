850a355feb18ff535a89a11a617dfb97fda14db0
Soren Goyal <sogoyal@expedia.com>
2017-09-12 15:56:20 -0500

    Fixed a logic for sorting algo

--- Exercises/Exercise 1/sort_solution.py
+++ Exercises/Exercise 1/sort_solution.py
@@ -1,11 +1,11 @@
-l = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+l = [10, 9, 8, 7, 1, 5, 4, 3, 2, 6]
 
 for index in range(0, 10):
 	minIndex = index
 	for j in range(index, 10):
 		if(l[minIndex] >  l[j]):
 			minIndex = j
-		temp = l[index]
-		l[index] = l[minIndex]
-		l[minIndex] = temp
+	temp = l[index]
+	l[index] = l[minIndex]
+	l[minIndex] = temp
 print(l[:10])
